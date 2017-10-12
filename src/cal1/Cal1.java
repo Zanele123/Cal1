fbf8c55f7feedae33e6eb12290c7c0f532ffdf53
Student <Student@ZAPRE-D00000166.DirectDelivery.com>
2017-10-12 10:50:56 +0200

    Additional work added 1

---
+++ src/cal1/Cal1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cal1;
+
+/**
+ *
+ * @author Student
+ */
+public class Cal1 {
+
+  public int Add (String s1, String s2) {
+      int d1 = Integer.parseInt(s1);
+      int d2 = Integer.parseInt(s2);
+      return d1 + d2;
+  }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
